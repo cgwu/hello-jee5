645419164c0f158319c71acf28739d964ed19f1c
gg <gg.wcg@qq.com>
2014-09-20 11:53:57 +0800

    convention

---
+++ src/main/java/struts2InAction/actions/test/ProductAction.java
@@ -0,0 +1,23 @@
+package struts2InAction.actions.test;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Namespace("/test/product")
+public class ProductAction extends ActionSupport {
+	private static final long serialVersionUID = -482341107997883758L;
+
+	@Action("add")
+	public String add() {
+		System.out.println("product add");
+		return "add";
+	}
+	@Action("list")
+	public String list() {
+		System.out.println("product list");
+		return "list";
+	}
+
+}
---
+++ src/main/webapp/jsp/test/product/add.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+<title>product add</title>
+</head>
+<body>
+<h1>product add</h1>
+</body>
+</html>
