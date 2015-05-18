66f3745eb0c81660ea04d4ad8707f88b4fc58c86
gg <gg.wcg@qq.com>
2015-05-18 18:57:30 +0800

    activiti测试初步成功。

---
+++ src/test/java/com/cg/test/activiti/TestBookOrder.java
@@ -0,0 +1,44 @@
+package com.cg.test.activiti;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.Test;
+
+public class TestBookOrder {
+
+	@Test
+	public void test() {
+		ProcessEngine processEngine = ProcessEngineConfiguration
+				.createStandaloneProcessEngineConfiguration().buildProcessEngine();
+		
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		IdentityService identityService = processEngine.getIdentityService();
+		TaskService taskService = processEngine.getTaskService();
+		
+		repositoryService.createDeployment().addClasspathResource("diagrams/bookorder.bpmm").deploy();
+		Map<String, Object> variableMap = new HashMap<String, Object>();
+		variableMap.put("isbn", "123456");
+		identityService.setAuthenticatedUserId("kermit");
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("bookorder",
+				variableMap);
+		assertNotNull(processInstance.getId());
+		List<Task> taskList = taskService.createTaskQuery().taskCandidateUser("kermit").list();
+		assertEquals(1, taskList.size());
+		System.out.println("found task " + taskList.get(0).getName());
+		taskService.complete(taskList.get(0).getId());
+	}
+
+}
---
+++ src/test/java/com/cg/test/activiti/TestSayHello.java
@@ -0,0 +1,29 @@
+package com.cg.test.activiti;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.junit.Test;
+
+public class TestSayHello {
+
+	@Test
+	public void test() {
+		ProcessEngine processEngine = ProcessEngineConfiguration
+				.createStandaloneInMemProcessEngineConfiguration().buildProcessEngine();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		repositoryService.createDeployment().addClasspathResource("diagrams/sayhello.bpmn")
+				.deploy();
+		ProcessInstance processInstance = runtimeService
+				.startProcessInstanceByKey("helloworld");
+		assertNotNull(processInstance.getId());
+		System.out.println("id " + processInstance.getId() + " "
+				+ processInstance.getProcessDefinitionId());
+	}
+
+}
